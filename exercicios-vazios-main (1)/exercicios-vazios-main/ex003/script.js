e7f8d4c33b937b35cbf4d31f55fac6eab4df22c4
matheus2005hidalgo-arch <matheus2005hidalgo@gmail.com>
2025-11-24 21:22:14 -0300

    Add files via upload

---
+++ exercicios-vazios-main (1)/exercicios-vazios-main/ex003/script.js
@@ -0,0 +1,8 @@
+let numero1 = Number(prompt("Digite o primeiro numero"))
+let nummero2 = Number(prompt("Digite o segundo numero")) 
+
+if(numero1 === nummero2){
+    alert("São iguais")
+}else{
+    alert("não são iguais")
+}
